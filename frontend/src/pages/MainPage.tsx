aefdf3a7c445eff842e6cb36724228db6d28f987
RohitPithani026 <rohitpithani13@gmail.com>
2025-01-07 23:59:09 +0530

    Added some

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -31,13 +31,13 @@ export function MainPage() {
             <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
                 <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
                     <Link to={"/signin"}>
-                        Sign In
+                        login 
                     </Link>
 
                 </button>
                 <button className="w-40 h-10 rounded-xl bg-white text-black border border-black text-sm">
                     <Link to={"/signup"}>
-                        Sign Up
+                        register
                     </Link>
                 </button>
             </div>
